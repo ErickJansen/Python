dab56e2ca9d7606d0871cb1165ed021db971f5aa
ErickJansen <83081172+ErickJansen@users.noreply.github.com>
2021-04-28 14:09:18 -0300

    Add files via upload

---
+++ EX002 - RESPONDENDO AO USUARIO.py
@@ -0,0 +1,5 @@
+nome = input('Digite seu nome')
+idade = input('Digite sua idade')
+peso = input('Qual seu peso')
+
+print('É um prazer te conhecer {}!'.format(nome), 'Sua idade é: {}'.format(idade), 'E seu peso é: {}'.format(peso))
